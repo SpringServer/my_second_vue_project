f59e4e0a6405b5b6c7a3a36a7c0820f8d860b0eb
Spring <531894107@qq.com>
2018-12-26 17:39:07 +0800

    this is my second vue project author:mce

---
+++ src/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div>
+            <input type="text" v-model="inputValue">
+            <button v-on:click="handsubmit">提交</button>
+        </div>
+        <ul>
+            <todoitem v-for="(item,index) in list" v-bind:key="index" v-text="item" v-bind:index="index" v-on:moveList="spliceIt"></todoitem>
+        </ul>
+    </div>
+</template>
+<script>
+import list from './components/List'
+export default{
+    data:function(){
+        return {
+            inputValue:"",
+            list:[]
+        }
+    },
+    methods:{
+        handsubmit:function(){
+            this.list.push(this.inputValue);
+            //console.log(this.list)
+            this.inputValue="";
+        },
+        spliceIt:function(index){
+            this.list.splice(index,1)
+        },
+    },
+    components:{
+        'todoitem':list
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <li v-on:click="delList"></li>
+</template>
+
+<script>
+export default {
+ props:['index'],
+ methods: {
+   delList:function(){
+     this.$emit("moveList",this.index)
+   }
+ },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
